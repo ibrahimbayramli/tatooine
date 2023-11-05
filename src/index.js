368cbd9a2f8bf3837393b4fc465fb9b22907e6bd
İbrahim BAYRAMLI <54009509+ibrahimbayramli@users.noreply.github.com>
2023-11-05 22:54:58 +0300

    project created and some methods write

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {logFile} from "./utils/logs";
+import nanoid from "./utils/nanoid";
+
+logFile("test",{
+    id:nanoid(),
+    name:"ibrahim"
+
+})
+
---
+++ src/utils/nanoid.js
@@ -0,0 +1,9 @@
+import {customAlphabet} from "nanoid";
+
+
+const nanoid=customAlphabet("0123456789abcdefghijklmnoprstuvyzABCDEFGHIJKLMNOPRSTUVYZ",20);
+
+export default nanoid;
+
+
+
